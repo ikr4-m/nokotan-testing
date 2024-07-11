9dfbecc0a0537361ecf0eedbecb155897f232dd1
Ikramullah <ikramullah@mplus.software>
2024-07-11 16:56:42 +0700

    add nokotan video

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { serveStatic } from '@hono/node-server/serve-static'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app
+  .use('/static/*', serveStatic({ root: './', }))
+  .get('/', c => c.redirect('/static/nokotan.mp4'))
 
 const port = 3000
 console.log(`Server is running on port ${port}`)
