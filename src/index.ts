08f9956958311f96aa79804d1e47dc217e857fa1
Ikramullah <ikramullah@mplus.software>
2024-07-12 10:12:26 +0700

    add logger

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
 import { serveStatic } from '@hono/node-server/serve-static'
 
 const app = new Hono()
 
 app
+  .use(logger())
   .use('/static/*', serveStatic({ root: './', }))
   .get('/', c => c.redirect('/static/nokotan.mp4'))
 
 const port = 3000
-console.log(`Server is running on port ${port}`)
+const hostname = "0.0.0.0"
+console.log(`Server is running on http://${hostname}:${port}`)
 
 serve({
   fetch: app.fetch,
   port,
-  hostname: "0.0.0.0"
+  hostname
 })
